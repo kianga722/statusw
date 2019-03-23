f034cc63a19acdf1b02f0b8df7f12d8be12a2084
kianga722 <kianga722@gmail.com>
2019-03-23 19:26:36 -0400

    Add conditional if environment is production for dotenv

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 // const fs = require('fs');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const port = process.env.PORT || 8081;
 
