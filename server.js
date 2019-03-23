299320646faa4ce79f8e7ad0a06ac6fb6ee4be2c
kianga722 <kianga722@gmail.com>
2019-03-22 21:44:23 -0400

    Edit server file

--- server.js
+++ server.js
@@ -72,11 +72,6 @@ app.get('/api/', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-  // Handle React routing, return all requests to React app
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-  });
 }
 
 // start the server
